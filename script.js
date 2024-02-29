85594f75a3df57c9896713cb8f93845ef1892718
Maksa9905 <maksa9905@yandex.ru>
2024-02-29 19:47:53 +0300

    Добавил страницу с регистрацией, исправил анимацию для welcome страницы, начал делать страницу confirm-email

--- script.js
+++ script.js
@@ -1,12 +1,83 @@
-function showPage(pageNumber) {
-  var currentPage = document.querySelector(".active"); //page1 class="page active"
-  var nextPage = document.getElementById("page" + pageNumber); //page2 class="page"
+function showPage(pageId) {
+  var pageNumber = pageId.slice(-1);
+  var currentPage = document.querySelector(".active");
+  var nextPage = document.getElementById("page" + pageNumber);
 
-  if (pageNumber > currentPage.id.slice(-1)) {
-    nextPage.classList.add("active", "next");
-  } else {
-    nextPage.classList.add("active", "previous");
+  var previousPage = document.querySelector(".animation_slide_to_left");
+  if (previousPage == null)
+    previousPage = document.querySelector(".animation_slide_to_right");
+
+  if (previousPage != null) {
+    previousPage.classList.remove(
+      "animation_slide_to_left",
+      "animation_slide_to_right"
+    );
   }
 
-  currentPage.classList.remove("active", "next", "previous");
+  currentPage.classList.remove(
+    "active",
+    "animation_slide_to_right",
+    "animation_slide_from_left",
+    "animation_slide_to_left",
+    "animation_slide_from_right"
+  );
+
+  if (pageNumber > currentPage.id.slice(-1)) {
+    nextPage.classList.add("active", "animation_slide_from_right");
+    currentPage.classList.add("animation_slide_to_left");
+  } else {
+    nextPage.classList.add("active", "animation_slide_from_left");
+    currentPage.classList.add("animation_slide_to_right");
+  }
+}
+
+function animButtons(buttonNumber) {
+  var currentButton = document.querySelector(".but-active");
+  var nextButton = document.getElementById("but" + buttonNumber);
+
+  if (nextButton != currentButton) {
+    nextButton.classList.remove("animation_button_diactivate");
+    nextButton.classList.add("but-active", "animation_button_activate");
+    currentButton.classList.remove("but-active", "animation_button_activate");
+    currentButton.classList.add("animation_button_diactivate");
+  }
+}
+
+function nextBoard() {
+  var currentButton = document.querySelector(".but-active");
+
+  if (currentButton.id.slice(-1) == "3") {
+    showPage("page4");
+  }
+
+  var boardNumber = parseInt(currentButton.id.slice(-1)) + 1;
+  var nextButton = document.getElementById("but" + boardNumber);
+
+  nextButton.classList.remove("animation_button_diactivate");
+  nextButton.classList.add("but-active", "animation_button_activate");
+  currentButton.classList.remove("but-active", "animation_button_activate");
+  currentButton.classList.add("animation_button_diactivate");
+
+  var currentBoard = document.querySelector(".board-active");
+  var nextBoard = document.getElementById("board" + boardNumber);
+
+  if (nextBoard != currentBoard) {
+    nextBoard.classList.remove("animation_board_diactivate");
+    nextBoard.classList.add("board-active", "animation_board_activate");
+    currentBoard.classList.remove("board-active", "animation_board_activate");
+    currentBoard.classList.add("animation_board_diactivate");
+  }
+}
+
+function showBoard(boardId) {
+  var boardNumber = boardId.slice(-1);
+  var currentBoard = document.querySelector(".board-active");
+  var nextBoard = document.getElementById("board" + boardNumber);
+
+  if (nextBoard != currentBoard) {
+    nextBoard.classList.remove("animation_board_diactivate");
+    nextBoard.classList.add("board-active", "animation_board_activate");
+    currentBoard.classList.remove("board-active", "animation_board_activate");
+    currentBoard.classList.add("animation_board_diactivate");
+  }
 }
