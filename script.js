dfb1bb194f106cbe4e0ece29b8e9e4b018a2609c
Maksa9905 <maksa9905@yandex.ru>
2024-03-03 01:03:24 +0300

    Добавил проверку валидности Email'а и пароля на странице регистрации, дописал страницу email-confirm

--- script.js
+++ script.js
@@ -81,3 +81,81 @@ function showBoard(boardId) {
     currentBoard.classList.add("animation_board_diactivate");
   }
 }
+
+// Проверка валидности Email; возвращает true - если значение email корректно, false - если некорректно
+function valideEmail(Email) {
+  let email__error_title = document.getElementById(
+    "signin-page__inner__email__error-title"
+  );
+  let flag;
+  const validateEmail =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (validateEmail.test(Email.value)) {
+    Email.style.backgroundColor = "#f1f1f1";
+    Email.style.borderColor = "#8b8b8b";
+    email__error_title.innerHTML = " ";
+    flag = true;
+  } else {
+    Email.style.backgroundColor = "#ffd6d6";
+    Email.style.borderColor = "red";
+    email__error_title.innerHTML = "Email введен неверно";
+    flag = false;
+  }
+
+  return flag;
+}
+
+function validePassword(password, confirm_password) {
+  let password__error_title = document.getElementById(
+    "signin-page__inner__password__error-title"
+  );
+  const validatePassword =
+    /^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/;
+
+  let flag = true;
+
+  console.log(validatePassword.test(password.value));
+  if (validatePassword.test(password.value)) {
+    if (password.value != confirm_password.value) {
+      password.style.borderColor = "red";
+      password.style.backgroundColor = "#ffd6d6";
+      confirm_password.style.borderColor = "red";
+      confirm_password.style.backgroundColor = "#ffd6d6";
+      flag = false;
+      password__error_title.innerHTML = "Пароли не совпадают";
+    } else {
+      password.style.borderColor = "#8b8b8b";
+      password.style.backgroundColor = "#f1f1f1";
+      confirm_password.style.borderColor = "#8b8b8b";
+      confirm_password.style.backgroundColor = "#f1f1f1";
+      password__error_title.innerHTML = " ";
+    }
+  } else {
+    password__error_title.innerHTML =
+      "Пароль должен содержать минимум 8 символов, включая строчные и заглавные буквы латинского алфавита, числа 0-9, и специальные знаки !#$%&?";
+    flag = false;
+    password.style.borderColor = "red";
+    password.style.backgroundColor = "#ffd6d6";
+    confirm_password.style.borderColor = "red";
+    confirm_password.style.backgroundColor = "#ffd6d6";
+  }
+
+  return flag;
+}
+
+function sendEmail() {
+  let Email = document.getElementById("signin-page__email-block__email");
+  let password = document.getElementById("signin-page__email-block__password");
+  let confirm_password = document.getElementById(
+    "signin-page__email-block__confirm-password"
+  );
+
+  valideEmail(Email);
+  validePassword(password, confirm_password);
+  if (valideEmail(Email) && validePassword(password, confirm_password)) {
+    document.getElementById("confirm-email-page__inner__email").innerHTML =
+      Email.value;
+    showPage("page5");
+  }
+}
